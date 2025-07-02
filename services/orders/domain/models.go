cc6505101d4baf3c9f7f2a3b70682ab01acd57a3
Wrtgvr <saldaniil123@gmail.com>
2025-07-02 11:05:35 +0700

    Added: Validation funcs

--- services/orders/domain/models.go
+++ services/orders/domain/models.go
@@ -1,10 +1,9 @@
 package domain
 
-type CreateOrderParams struct {
-	CustomerID uint32
-	ProductID  uint32
-	Quantity   uint32
-}
+import (
+	"errors"
+	"strings"
+)
 
 type Order struct {
 	OrderID    uint32
@@ -12,4 +11,55 @@ type Order struct {
 	ProductID  uint32
 	Quantity   uint32
 	Status     string
+}
+
+// Status consts
+
+func (o Order) Validate() error {
+	errs := make([]string, 0, 5)
+
+	if o.OrderID <= 0 {
+		errs = append(errs, "invalid order id")
+	}
+	if o.CustomerID <= 0 {
+		errs = append(errs, "invalid customer id")
+	}
+	if o.ProductID <= 0 {
+		errs = append(errs, "invalid product id")
+	}
+	if o.Quantity <= 0 {
+		errs = append(errs, "invalid quantity")
+	}
+
+	if len(errs) > 0 {
+		return errors.New(strings.Join(errs, ", "))
+	}
+
+	return nil
+}
+
+type CreateOrderParams struct {
+	CustomerID uint32
+	ProductID  uint32
+	Quantity   uint32
+}
+
+func (p CreateOrderParams) Validate() error {
+	errs := make([]string, 0, 3)
+
+	if p.CustomerID <= 0 {
+		errs = append(errs, "invalid customer id")
+	}
+	if p.ProductID <= 0 {
+		errs = append(errs, "invalid product id")
+	}
+	if p.Quantity <= 0 {
+		errs = append(errs, "invalid quantity")
+	}
+
+	if len(errs) > 0 {
+		return errors.New(strings.Join(errs, ", "))
+	}
+
+	return nil
 }
